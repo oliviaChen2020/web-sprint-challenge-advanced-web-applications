8dce172bea732dba2d787d94261b78ca50098971
Olivia <olivia@MacBook-Pro.local>
2020-11-13 12:52:09 -0500

    MVP partially met

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,29 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
+import Bubbles from './Bubbles';
+import ColorList from './ColorList';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+    fetchColor();
+  }, []);
+  const fetchColor = () => {
+    axiosWithAuth()
+      .get('http://localhost:5000/api/colors')
+      .then((res) => {
+        console.log(res.data);
+        setColorList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <>
